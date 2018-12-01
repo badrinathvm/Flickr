568f9671c05b2cb82f3eaa02d5d85e4de52fa71b
Badarinath Venkatnarayansetty <bvenkatnarayansetty@evernote.com>
2018-12-01 08:14:03 -0800

    Initial commit and folder re organisation for Contrtoller , added navigation controller from AppDelegate

---
+++ Flickr/Controller/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Badarinath Venkatnarayansetty on 12/1/18.
+//  Copyright © 2018 Badarinath Venkatnarayansetty. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
